60efbb3bf90159a6ea5e1a703e3325b86f7bbe67
Dowgen <303944689@qq.com>
2018-01-02 15:40:10 +0800

    debug

--- src/views/Square/sqProjectDetail.vue
+++ src/views/Square/sqProjectDetail.vue
@@ -166,6 +166,7 @@ export default {
   },
   beforeRouteLeave(to, from, next) {
     var self = this;
+    alert(this.wechatShareReturnLink)
     //微信分享设置
     Lib.M.ajax({
       url : '/wechat/wxSig',
@@ -174,13 +175,14 @@ export default {
         if(res.code==200){
           let wxSig = res.data;
           wx.config({
-            debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+            debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
             appId: wxSig.appid, // 必填，公众号的唯一标识
             timestamp: wxSig.timestamp, // 必填，生成签名的时间戳
             nonceStr:  wxSig.noncestr, // 必填，生成签名的随机串
             signature: wxSig.signature,   // 必填，签名，见附录1
             jsApiList: ["onMenuShareTimeline","onMenuShareAppMessage"] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
           });
+          alert(self.wechatShareReturnLink)
           //微信分享设置
           wx.onMenuShareTimeline({
             title: '51资金资产', 
