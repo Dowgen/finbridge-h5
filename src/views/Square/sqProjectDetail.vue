0e102426a40042f3198f6975bfbabb4fc7414746
Dowgen <303944689@qq.com>
2018-01-03 11:02:44 +0800

    debug

--- src/views/Square/sqProjectDetail.vue
+++ src/views/Square/sqProjectDetail.vue
@@ -173,7 +173,7 @@ export default {
         if(res.code==200){
           let wxSig = res.data;
           wx.config({
-            debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+            debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
             appId: wxSig.appid, // 必填，公众号的唯一标识
             timestamp: wxSig.timestamp, // 必填，生成签名的时间戳
             nonceStr:  wxSig.noncestr, // 必填，生成签名的随机串
@@ -323,7 +323,7 @@ export default {
             let params = '&fromShare=y';
             if(self.$route.query.fromShare=='y') params = '';
             wx.config({
-              debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+              debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
               appId: wxSig.appid, // 必填，公众号的唯一标识
               timestamp: wxSig.timestamp, // 必填，生成签名的时间戳
               nonceStr:  wxSig.noncestr, // 必填，生成签名的随机串
