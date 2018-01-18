15009a353f379f6d821a71b537ae35134ddb47fc
Dowgen <303944689@qq.com>
2018-01-18 19:35:19 +0800

    debug

--- src/views/Square/sqProjectDetail.vue
+++ src/views/Square/sqProjectDetail.vue
@@ -399,27 +399,26 @@ export default {
           if(res.code==200){
             let wxSig = res.data;
             //分享链接设置
-            let url = '';
+            let url,shareUrl = '';
             if(self.$route.query.fromShare=='y'){
               url = url.substr(0, url.indexOf('?')) +url.substr(url.indexOf('#'),url.length)
             }else{
               url = location.href + '&fromShare=y';
             }
+            shareUrl = url;
             
             wx.config({
-              debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+              debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
               appId: wxSig.appid, // 必填，公众号的唯一标识
               timestamp: wxSig.timestamp, // 必填，生成签名的时间戳
               nonceStr:  wxSig.noncestr, // 必填，生成签名的随机串
               signature: wxSig.signature,   // 必填，签名，见附录1
               jsApiList: ["onMenuShareTimeline","onMenuShareAppMessage"] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
             });
-            alert(self.$route.query.fromShare);
-            alert(location.href + params);
             //微信分享设置
             wx.onMenuShareTimeline({
               title: self.info.projectName, 
-              link:  location.href + params, 
+              link:  shareURL, 
               imgUrl: Lib.M.webDomain+'/logo.png', 
               success: function () { 
                 self.shareProject();
@@ -432,7 +431,7 @@ export default {
             wx.onMenuShareAppMessage({
               title: self.info.projectName, 
               desc: '关注51资金资产公众号，获取更多信息', 
-              link:  location.href + params,
+              link:  shareURL,
               imgUrl: Lib.M.webDomain+'/logo.png', 
               /*type: '', // 分享类型,music、video或link，不填默认为link*/
               /*dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空*/
